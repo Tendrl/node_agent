b86118236876763ad23878d25cfc79754db289c6
GowthamShanmugasundaram <gshanmug@redhat.com>
2018-10-11 13:40:10 +0000

    Testcase fixes
    bugzilla: 1637977
    tendrl-bug-id:Tendrl/node-agent#852
    
    Signed-off-by: GowthamShanmugasundaram <gshanmug@redhat.com>

--- tendrl/node_agent/tests/test_tendrl_glusterfs_brick_utilization.py
+++ tendrl/node_agent/tests/test_tendrl_glusterfs_brick_utilization.py
@@ -1,7 +1,7 @@
 import etcd
 import importlib
 import json
-import socket
+import maps
 import sys
 
 from mock import MagicMock
@@ -21,73 +21,28 @@ del sys.modules['collectd']
 del sys.modules['tendrl_gluster']
 
 
-class TestTendrlGlusterfsBrickUtilization(object):
-    @patch("tendrl.node_agent.monitoring.collectd.collectors."
-           "gluster.utils.exec_command")
-    @patch("tendrl.node_agent.monitoring.collectd.collectors."
-           "gluster.utils.ini2json")
-    @patch("subprocess.Popen.communicate")
-    @patch("tendrl.node_agent.monitoring.collectd.collectors."
-           "gluster.low_weight.tendrl_glusterfs_brick_utilization."
-           "os.statvfs")
-    @patch("tendrl.node_agent.monitoring.collectd.collectors."
-           "gluster.low_weight.tendrl_glusterfs_brick_utilization."
-           "os.path.ismount")
-    @patch("tendrl.node_agent.monitoring.collectd.collectors."
-           "gluster.low_weight.tendrl_glusterfs_brick_utilization."
-           "os.path.realpath")
-    @patch("tendrl.node_agent.monitoring.collectd.collectors."
-           "gluster.utils.find_brick_host")
-    @patch.object(etcd, "Client")
-    @patch.object(socket, "gethostbyname")
-    def test_gluster_brick_utilization(self,
-                                       gethostbyname,
-                                       client,
-                                       brick_host,
-                                       dirname,
-                                       ismount,
-                                       statvfs,
-                                       communicate,
-                                       ini2json,
-                                       execu):
-        gethostbyname.return_value = "10.70.41.169"
-        client.return_value = etcd.Client()
-        brick_host.return_value = "10.70.41.169"
-        gluster_state = mock_gluster_state.gluster_state()
-        execu.return_value = ("pytest", "")
-        ini2json.ini_to_dict.return_value = gluster_state
-        obj = brick_utilization.TendrlBrickUtilizationPlugin()
-        obj.CLUSTER_TOPOLOGY = utils.get_gluster_cluster_topology()
-        obj.parse_proc_mounts = MagicMock(
-            return_value=self.parse_proc_mounts()
-        )
-        communicate.return_value = self.communicate()
-        ismount.return_value = True
-        dirname.return_value = (['/'])
-        mock_obj = MagicMock()
-        mock_obj.f_bsize = 4096
-        mock_obj.f_frsize = 4096
-        mock_obj.f_blocks = 2093568
-        mock_obj.f_bfree = 1351644
-        mock_obj.f_bavail = 1351644
-        mock_obj.f_files = 4192256
-        mock_obj.f_ffree = 4101251
-        mock_obj.f_favail = 4101251
-        mock_obj.f_flag = 4096
-        mock_obj.f_namemax = 255
-        statvfs.return_value = mock_obj
-        with open("tendrl/node_agent/tests/output/"
-                  "tendrl_glusterfs_brick_utilization_output.json"
-                  ) as f:
-            assert obj.get_metrics() == json.loads(f.read())
-
-    def communicate(self):
-        with open("tendrl/node_agent/tests/"
-                  "samples/lvs_sample.txt") as f:
-            return f.read(), None
-
-    def parse_proc_mounts(self):
+def read(param):
+    if "data" in param.split("/")[-1]:
         with open(
             "tendrl/node_agent/tests/samples/"
-                "mount_point_state_sample.json") as f:
-            return json.load(f)
+                "brick_utilization.json") as f:
+            return maps.NamedDict(value=f.read())
+    else:
+        key = "/clusters/%s/Bricks/all/%s" % (
+            "7bccda8c-8c85-45a8-8be0-3f71f4db7db7",
+            "10.70.41.169"
+        )
+        return maps.NamedDict(leaves=[maps.NamedDict(key=key)])
+
+
+class TestTendrlGlusterfsBrickUtilization(object):
+    @patch.object(etcd, "Client")
+    def test_gluster_brick_utilization(self, client):
+        client = etcd.Client()
+        client.return_value = client
+        with patch.object(client, "read", read):
+            obj = brick_utilization.TendrlBrickUtilizationPlugin()
+            with open("tendrl/node_agent/tests/output/"
+                      "tendrl_glusterfs_brick_utilization_output.json"
+                      ) as f:
+                assert obj.get_metrics() == json.loads(f.read())
